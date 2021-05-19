f5334dddd7cb0620a66dc270a64326e22731304f
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-19 16:36:08 +0200

    Update app.js

--- api/dist/app.js
+++ api/dist/app.js
@@ -1,24 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-const createError = require("http-errors");
-const express = require("express");
-const path_1 = require("path");
-// @ts-ignore
-const cookieParser = require("cookie-parser");
-// @ts-ignore
-const logger = require("morgan");
-// @ts-ignore
-const Magic = require("express-routemagic");
+import { join } from 'path';
 const app = express();
 // view engine setup
-app.set('views', path_1.join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path_1.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 Magic.use(app, { invokerPath: __dirname }); // need to use `invokerPath` because we are not in root dir.
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
